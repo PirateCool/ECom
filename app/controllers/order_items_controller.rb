c2642311bcef9c22d51bcaba13b3b934210e6cba
zOmegad <simonrichard1998@gmail.com>
2018-11-29 21:22:24 +0100

    order item controller works

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,17 +1,16 @@
 class OrderItemsController < ApplicationController
 
   def create
-  	puts "coucou"
     @order = current_order
     @item = @order.order_items.new(item_params)
     @order.save
     session[:order_id] = @order.id
-    redirect_to products_path
+    redirect_to items_path
   end
 
   private
 
   def item_params
-    params.require(:order_item).permit(:quantity, :item_id)
+    params.require(:order_item).permit(:quantity, :product_id)
   end
 end
